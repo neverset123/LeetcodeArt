b709b6a86cbec981d1139209d7c9d6a656f0e9df
neverset <jiao.jiao@isi.fraunhofer.de>
2023-02-09 01:10:32 +0100

    dbt

--- leetcode/binary_trees/BST.py
+++ leetcode/binary_trees/BST.py
@@ -37,12 +37,59 @@ class Solution:
         self.traverse(root.left)
         if self.preNode!=None and self.preNode.val>=root.val:
             self.valid=False
+        print(root.val)
         self.preNode=root
         self.isValidBST(root.right)
 
+    def search(self, root, target):
+        if root==None:
+            return
+        if root.val==target:
+            return root
+        elif root.val>target:
+            return self.search(root.left, target)
+        else:
+            return self.search(root.right, target)
+
+    def insert(self, root, val):
+        if root==None:
+            return TreeNode(val)
+        if root.val>val:
+            root.left=self.insert(root.left, val)
+        elif root.val<val:
+            root.right=self.insert(root.right, val)
+        return root
+
+    def delete(self, root, val):
+        if root==None:
+            return
+        if root.val==val:
+            if root.left==None:
+                return root.right
+            if root.right==None:
+                return root.left
+            else:
+                node_next=self.min(root.right)
+                root.right=self.delete(root.right, node_next.val)
+                #root.val=node_next.val
+                node_next.left=root.left
+                node_next.right=root.right
+                root=node_next
+        elif root.val>val:
+            root.left=self.delete(root.left, val)
+        else:
+            root.right=self.delete(root.right, val)
+        return root
+    
+    def min(self, root):
+        if root==None:
+            return
+        return root if root.left == None else self.min(root.left)
 
 if __name__ == "__main__":
-    test_data = [10,5,15,None,None,6,20]
+    test_data = [5,3,6,2,4,None,None,1]
     test_tree = from_list(test_data)
     solution = Solution()
-    print(solution.isValidBST(test_tree))
+    #solution.traverse(test_tree)
+    solution.delete(test_tree, 2)
+    solution.traverse(test_tree)
