8d9a3cb387c2b9c6b2f90d7d01b00deb5559c4fd
neverset <jiao.jiao@isi.fraunhofer.de>
2023-03-11 18:11:48 +0100

    update dynamic programming

---
+++ leetcode/dynamic_programming/max_subarray.py
@@ -0,0 +1,12 @@
+import sys
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp=[-sys.maxsize - 1]*len(nums)
+        dp[0]=nums[0]
+        for i in range(1,len(nums)):
+            dp[i]=max(nums[i], dp[i-1]+nums[i])
+        res=dp[0]
+        for i in range(len(nums)):
+            res=max(res, dp[i])
+        return res
--- leetcode/dynamic_programming/min_edit_distance.py
+++ leetcode/dynamic_programming/min_edit_distance.py
@@ -1,9 +1,9 @@
 #dp记录在状态(i，j)下已进行的操作数
-#有错误不知道为啥
+
 import sys
 class Solution:
     def minDistance(self, s1: str, s2: str) -> int:
-        memo=[[sys.maxsize]*(len(s2))]*(len(s1))
+        memo=[[sys.maxsize]*(len(s2)) for _ in range(len(s1))]
 
         def dp(s1, s2, i, j):
             if i==-1:
@@ -27,3 +27,22 @@ class Solution:
             return memo[i][j]
         
         return dp(s1,s2,len(s1)-1, len(s2)-1)
+    
+    def minDistance(self, s1: str, s2: str) -> int:
+        m,n=len(s1),len(s2)
+        dp=[[0]*(n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            dp[i][0]=i
+        for j in range(1, n+1):
+            dp[0][j]=j
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=min(dp[i-1][j]+1,
+                                     dp[i][j-1]+1,
+                                     dp[i-1][j-1]+1)
+        return dp[m][n]
+    
+    
